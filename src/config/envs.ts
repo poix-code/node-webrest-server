ae263b3eba4653ddce8410fd347b0fb9ebd0eb72
poix-code <juanuribe513@gmail.com>
2026-03-18 17:36:56 -0500

    Adding a defaul PORT to test

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { get } from 'env-var';
 
 export const envs = {
-    PORT: get('PORT').required().asPortNumber(),
+    PORT: get('PORT').required().default(3000).asPortNumber(),
     PUBLIC_PATH: get('PUBLIC_PATH').default('public').asString(),
     POSTGRES_URL: get('POSTGRES_URL').required().asString(),
 };
